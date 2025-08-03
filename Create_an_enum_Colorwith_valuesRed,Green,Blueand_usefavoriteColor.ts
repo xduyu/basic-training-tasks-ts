258722193f2b065a78a564ba55de98c8add2384b
rodya <rdstmn11@gmail.com>
2025-08-03 21:08:26 +0500

    n - f - test

---
+++ Create_an_enum_Colorwith_valuesRed,Green,Blueand_usefavoriteColor.ts
@@ -0,0 +1,9 @@
+enum Color {
+    Red = "Red",
+    Green = "Green",
+    Blue = "Blue"
+}
+
+
+const favoriteColor: Color = Color.Green;
+console.log(`My favorite color is: ${favoriteColor}`); // My favorite color is: Green
